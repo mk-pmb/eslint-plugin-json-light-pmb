fa3ae9aae1044a7fba1b8c3529d29fd34e3a093b
mk-pmb <mk-pmb@users.noreply.github.com>
2022-03-25 20:22:03 +0100

    Declare future eslint rules

--- test/depsHelper.js
+++ test/depsHelper.js
@@ -1,10 +1,8 @@
-﻿/* -*- tab-width: 2 -*- */
+/* -*- tab-width: 2 -*- */
 'use strict';
 
 module.exports = (function depsHelper(require) {
   return [
-    require('eslint-config-airbnb-base'),
-    require('eslint-plugin-import'),
     require('eslint-plugin-mocha'),
     require('mocha'),
   ];
